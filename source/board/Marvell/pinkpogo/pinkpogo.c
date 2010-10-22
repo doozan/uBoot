67fdd2c92ed5915638e51319b0a5600bc33bb4c0
Jeff <jeff@doozan.com>
2010-10-22 18:02:08 -0400

    Refactor code, add support for arcNumber, include GoflexNet and PinkPogo

---
+++ source/board/Marvell/pinkpogo/pinkpogo.c
@@ -0,0 +1,354 @@
+/*
+ * (C) Copyright 2009
+ * Marvell Semiconductor <www.marvell.com>
+ * Written-by: Prafulla Wadaskar <prafulla@marvell.com>
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA
+ */
+
+#include <common.h>
+#include <miiphy.h>
+#include <asm/arch/kirkwood.h>
+#include <asm/arch/mpp.h>
+#include <linux/ctype.h> /* isspace */
+#include <status_led.h>
+#include "pinkpogo.h"
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int board_init(void)
+{
+	/*
+	 * default gpio configuration
+	 * There are maximum 64 gpios controlled through 2 sets of registers
+	 * the  below configuration configures mainly initial LED status
+	 */
+	kw_config_gpio(PINKPOGO_OE_VAL_LOW,
+			PINKPOGO_OE_VAL_HIGH,
+			PINKPOGO_OE_LOW, PINKPOGO_OE_HIGH);
+
+	/* Multi-Purpose Pins Functionality configuration */
+	u32 kwmpp_config[] = {
+		MPP0_NF_IO2,
+		MPP1_NF_IO3,
+		MPP2_NF_IO4,
+		MPP3_NF_IO5,
+		MPP4_NF_IO6,
+		MPP5_NF_IO7,
+		MPP6_SYSRST_OUTn,
+		MPP7_GPO,
+		MPP8_UART0_RTS,
+		MPP9_UART0_CTS,
+		MPP10_UART0_TXD,
+		MPP11_UART0_RXD,
+		MPP12_SD_CLK,
+		MPP13_SD_CMD,
+		MPP14_SD_D0,
+		MPP15_SD_D1,
+		MPP16_SD_D2,
+		MPP17_SD_D3,
+		MPP18_NF_IO0,
+		MPP19_NF_IO1,
+		MPP20_GPIO,
+		MPP21_GPIO,
+		MPP22_GPIO,
+		MPP23_GPIO,
+		MPP24_GPIO,
+		MPP25_GPIO,
+		MPP26_GPIO,
+		MPP27_GPIO,
+		MPP28_GPIO,
+		MPP29_TSMP9,
+		MPP30_GPIO,
+		MPP31_GPIO,
+		MPP32_GPIO,
+		MPP33_GPIO,
+		MPP34_GPIO,
+		MPP35_GPIO,
+		MPP36_GPIO,
+		MPP37_GPIO,
+		MPP38_GPIO,
+		MPP39_GPIO,
+		MPP40_GPIO,
+		MPP41_GPIO,
+		MPP42_GPIO,
+		MPP43_GPIO,
+		MPP44_GPIO,
+		MPP45_GPIO,
+		MPP46_GPIO,
+		MPP47_GPIO,
+		MPP48_GPIO,
+		MPP49_GPIO,
+		0
+	};
+	kirkwood_mpp_conf(kwmpp_config);
+
+	/* adress of boot parameters */
+	gd->bd->bi_boot_params = kw_sdram_bar(0) + 0x100;
+
+return 0;
+}
+
+int misc_init_r()
+{
+  int __machine_arch_type;
+  char *str_arc_number = getenv("arcNumber");
+
+  if (str_arc_number) {
+    __machine_arch_type = simple_strtoul(str_arc_number, NULL, 10);
+    if (!__machine_arch_type)
+      __machine_arch_type = MACH_TYPE_DOCKSTAR; // Pink Pogo does not have its own machine ID
+  }
+
+	/*
+	 * arch number of board
+	 */
+	gd->bd->bi_arch_number = __machine_arch_type;
+}
+
+int dram_init(void)
+{
+	int i;
+
+	for (i = 0; i < CONFIG_NR_DRAM_BANKS; i++) {
+		gd->bd->bi_dram[i].start = kw_sdram_bar(i);
+		gd->bd->bi_dram[i].size = kw_sdram_bs(i);
+	}
+	return 0;
+}
+
+#ifdef CONFIG_RESET_PHY_R
+/* Configure and enable MV88E1116 PHY */
+void reset_phy(void)
+{
+	u16 reg;
+	u16 devadr;
+	char *name = "egiga0";
+
+	if (miiphy_set_current_dev(name))
+		return;
+
+	/* command to read PHY dev address */
+	if (miiphy_read(name, 0xEE, 0xEE, (u16 *) &devadr)) {
+		printf("Err..%s could not read PHY dev address\n",
+			__FUNCTION__);
+		return;
+	}
+
+	/*
+	 * Enable RGMII delay on Tx and Rx for CPU port
+	 * Ref: sec 4.7.2 of chip datasheet
+	 */
+	miiphy_write(name, devadr, MV88E1116_PGADR_REG, 2);
+	miiphy_read(name, devadr, MV88E1116_MAC_CTRL_REG, &reg);
+	reg |= (MV88E1116_RGMII_RXTM_CTRL | MV88E1116_RGMII_TXTM_CTRL);
+	miiphy_write(name, devadr, MV88E1116_MAC_CTRL_REG, reg);
+	miiphy_write(name, devadr, MV88E1116_PGADR_REG, 0);
+
+	/* reset the phy */
+	miiphy_reset(name, devadr);
+
+	printf("88E1116 Initialized on %s\n", name);
+}
+#endif /* CONFIG_RESET_PHY_R */
+
+
+static uint8_t saved_state[2] = {STATUS_LED_OFF, STATUS_LED_OFF};
+static uint8_t saved_blink_state[2] = {STATUS_LED_OFF, STATUS_LED_OFF};
+static uint32_t gpio_pin[2] = {1 << (14 + STATUS_LED_GREEN),
+			       1 << (14 + STATUS_LED_RED)};
+
+inline void switch_LED_on(uint8_t led)
+{
+  struct kwgpio_registers *gpio = (struct kwgpio_registers *)KW_GPIO1_BASE;
+
+	writel(readl(&gpio->oe) & ~gpio_pin[led], &gpio->oe);
+	saved_state[led] = STATUS_LED_ON;
+}
+
+inline void switch_LED_off(uint8_t led)
+{
+  struct kwgpio_registers *gpio = (struct kwgpio_registers *)KW_GPIO1_BASE;
+
+	writel(readl(&gpio->oe) | gpio_pin[led], &gpio->oe);
+	saved_state[led] = STATUS_LED_OFF;
+}
+
+void red_LED_on(void)
+{
+	switch_LED_on(STATUS_LED_RED);
+}
+
+void red_LED_off(void)
+{
+	switch_LED_off(STATUS_LED_RED);
+}
+
+void green_LED_on(void)
+{
+	switch_LED_on(STATUS_LED_GREEN);
+}
+
+void green_LED_off(void)
+{
+	switch_LED_off(STATUS_LED_GREEN);
+}
+
+void __led_init(led_id_t mask, int state)
+{
+	__led_set(mask, state);
+}
+
+void __led_toggle(led_id_t mask)
+{
+	if (STATUS_LED_RED == mask) {
+		(saved_state[STATUS_LED_RED] == STATUS_LED_ON) ? red_LED_off() : red_LED_on();
+	} else if (STATUS_LED_GREEN == mask) {
+		(saved_state[STATUS_LED_GREEN] == STATUS_LED_ON) ? green_LED_off() : green_LED_on();
+	}
+}
+
+void __led_set(led_id_t mask, int state)
+{
+	if (STATUS_LED_RED == mask) {
+		(STATUS_LED_ON == state) ? red_LED_on() : red_LED_off();
+	} else if (STATUS_LED_GREEN == mask) {
+		(STATUS_LED_ON == state) ? green_LED_on() : green_LED_off();
+	}
+}
+
+inline void switch_LED_blink_on(uint8_t led)
+{
+  struct kwgpio_registers *gpio = (struct kwgpio_registers *)KW_GPIO1_BASE;
+
+	writel(readl(&gpio->blink_en) | gpio_pin[led], &gpio->blink_en);
+	saved_state[led] = STATUS_LED_ON;
+}
+
+inline void switch_LED_blink_off(uint8_t led)
+{
+  struct kwgpio_registers *gpio = (struct kwgpio_registers *)KW_GPIO1_BASE;
+
+	writel(readl(&gpio->blink_en) & ~gpio_pin[led], &gpio->blink_en);
+	saved_state[led] = STATUS_LED_OFF;
+}
+
+void red_LED_blink_on(void)
+{
+	switch_LED_blink_on(STATUS_LED_RED);
+}
+
+void red_LED_blink_off(void)
+{
+	switch_LED_blink_off(STATUS_LED_RED);
+}
+
+void green_LED_blink_on(void)
+{
+	switch_LED_blink_on(STATUS_LED_GREEN);
+}
+
+void green_LED_blink_off(void)
+{
+	switch_LED_blink_off(STATUS_LED_GREEN);
+}
+
+void set_LED(char *szStatus)
+{
+  uint8_t led    = -1;
+  uint8_t status = -1;
+
+  /* Convert string to lowercase, max len 32 */
+  int max = 32;
+  char *s = szStatus;
+  while (*s && max--)
+    *s++ = tolower(*s);
+  s = szStatus;
+
+  /* Start with everything off */
+  switch_LED_off(STATUS_LED_GREEN);
+  switch_LED_blink_off(STATUS_LED_GREEN);
+  switch_LED_off(STATUS_LED_RED);
+  switch_LED_blink_off(STATUS_LED_RED);
+
+  if (szStatus == NULL) 
+  {
+    red_LED_on();
+    red_LED_blink_on();
+    return;
+  }
+
+  if ( strncmp(s, "green", 5) == 0 )
+  {
+    s += 5;
+    led = STATUS_LED_GREEN;
+  }
+  else if ( strncmp(s, "orange", 6) == 0 ) 
+  {
+    s += 6;
+    led = STATUS_LED_RED;
+  }
+  else if ( strncmp(s, "red", 3) == 0 ) 
+  {
+    s += 3;
+    led = STATUS_LED_RED;
+  }
+
+  if (led == -1 ) return;
+
+  /* Skip Whitespace */
+  while ( isspace(*s) ) s++;
+
+  /* Commands like "green" or "orange" should just turn the light on */
+  if ( *s == NULL || ( strncmp(s, "on", 2) == 0 ) )
+    status = STATUS_LED_ON;
+  else if ( strncmp(s, "blink", 5) == 0 )
+    status = STATUS_LED_BLINKING;
+  /* Unknown commands should turn the light off */
+  else
+    status = STATUS_LED_OFF;
+
+  if (status == STATUS_LED_OFF)
+  {
+    switch_LED_off(led);
+    switch_LED_blink_off(led);
+  }
+  else
+  {
+    switch_LED_on(led);
+    if (status == STATUS_LED_BLINKING)
+      switch_LED_blink_on(led);
+  }
+}
+
+
+void show_boot_progress (int val)
+{
+  if (val < 0)
+    set_LED( getenv("led_error") );
+
+  /* Ethernet Init */
+  else if (val == 64)
+    set_LED( getenv("led_init") );
+
+  /* Passing control to an image */
+  else if (val == 15)
+    set_LED( getenv("led_exit") );
+}
+
---
+++ source/include/configs/goflexnet.h
@@ -0,0 +1,289 @@
+/*
+ * (C) Copyright 2009
+ * Marvell Semiconductor <www.marvell.com>
+ * Written-by: Prafulla Wadaskar <prafulla@marvell.com>
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA
+ */
+
+#ifndef _CONFIG_GOFLEXNET_H
+#define _CONFIG_GOFLEXNET_H
+
+/*
+ * Version number information
+ */
+#define CONFIG_IDENT_STRING	"\nMarvell-GoflexNet by Jeff Doozan, Peter Carmichael"
+
+/*
+ * High Level Configuration Options (easy to change)
+ */
+#define CONFIG_MARVELL		1
+#define CONFIG_ARM926EJS	1	/* Basic Architecture */
+#define CONFIG_FEROCEON_88FR131	1	/* CPU Core subversion */
+#define CONFIG_KIRKWOOD		1	/* SOC Family Name */
+#define CONFIG_KW88F6281	1	/* SOC Name */
+#define CONFIG_MACH_GOFLEXNET	/* Machine type */
+
+#define CONFIG_MD5	/* get_random_hex on krikwood needs MD5 support */
+#define CONFIG_SKIP_LOWLEVEL_INIT	/* disable board lowlevel_init */
+#define CONFIG_KIRKWOOD_EGIGA_INIT	/* Enable GbePort0/1 for kernel */
+#define CONFIG_KIRKWOOD_RGMII_PAD_1V8	/* Set RGMII Pad voltage to 1.8V */
+
+/*
+ * CLKs configurations
+ */
+#define CONFIG_SYS_HZ		1000
+
+/*
+ * NS16550 Configuration
+ */
+#define CONFIG_SYS_NS16550
+#define CONFIG_SYS_NS16550_SERIAL
+#define CONFIG_SYS_NS16550_REG_SIZE	(-4)
+#define CONFIG_SYS_NS16550_CLK		CONFIG_SYS_TCLK
+#define CONFIG_SYS_NS16550_COM1		KW_UART0_BASE
+
+/*
+ * Serial Port configuration
+ * The following definitions let you select what serial you want to use
+ * for your console driver.
+ */
+
+#define CONFIG_CONS_INDEX	1	/*Console on UART0 */
+#define CONFIG_BAUDRATE			115200
+#define CONFIG_SYS_BAUDRATE_TABLE	{ 9600, 19200, 38400, 57600, \
+					  115200,230400, 460800, 921600 }
+/* auto boot */
+#define CONFIG_BOOTDELAY	3	/* default enable autoboot */
+
+/*
+ * For booting Linux, the board info and command line data
+ * have to be in the first 8 MB of memory, since this is
+ * the maximum mapped by the Linux kernel during initialization.
+ */
+#define CONFIG_CMDLINE_TAG	1	/* enable passing of ATAGs  */
+#define CONFIG_INITRD_TAG	1	/* enable INITRD tag */
+#define CONFIG_SETUP_MEMORY_TAGS 1	/* enable memory tag */
+
+#define	CONFIG_SYS_PROMPT	"Marvell>> "	/* Command Prompt */
+#define	CONFIG_SYS_CBSIZE	1024	/* Console I/O Buff Size */
+#define	CONFIG_SYS_PBSIZE	(CONFIG_SYS_CBSIZE \
+		+sizeof(CONFIG_SYS_PROMPT) + 16)	/* Print Buff */
+
+/*
+ * Commands configuration
+ */
+#define CONFIG_SYS_NO_FLASH		/* Declare no flash (NOR/SPI) */
+#include <config_cmd_default.h>
+#define CONFIG_CMD_AUTOSCRIPT
+#define CONFIG_CMD_DHCP
+#define CONFIG_CMD_ENV
+#define CONFIG_CMD_MII
+#define CONFIG_CMD_NAND
+#define CONFIG_CMD_PING
+#define CONFIG_CMD_USB
+#define CONFIG_SYS_LONGHELP
+#define CONFIG_PREBOOT
+#define CONFIG_SYS_HUSH_PARSER
+#define CONFIG_SYS_PROMPT_HUSH_PS2 "> "
+#define CONFIG_CMD_IDE
+/*
+ * NAND configuration
+ */
+#ifdef CONFIG_CMD_NAND
+#define CONFIG_NAND_KIRKWOOD
+#define CONFIG_SYS_MAX_NAND_DEVICE	1
+#define NAND_MAX_CHIPS			1
+#define CONFIG_SYS_NAND_BASE		0xD8000000	/* KW_DEFADR_NANDF */
+#define NAND_ALLOW_ERASE_ALL		1
+#endif
+
+/*
+ *  Environment variables configurations
+ */
+#ifdef CONFIG_CMD_NAND
+#define CONFIG_ENV_IS_IN_NAND		1
+#define CONFIG_ENV_SECT_SIZE		0x20000	/* 128K */
+#else
+#define CONFIG_ENV_IS_NOWHERE		1	/* if env in SDRAM */
+#endif
+/*
+ * max 4k env size is enough, but in case of nand
+ * it has to be rounded to sector size
+ */
+#define CONFIG_ENV_SIZE			0x20000	/* 128k */
+#define CONFIG_ENV_ADDR			0xc0000
+#define CONFIG_ENV_OFFSET		0xc0000	/* env starts here */
+
+/*
+ * Default environment variables
+ */
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+  "arcNumber=2097\0" \
+  "mainlineLinux=yes\0" \
+  "console=ttyS0,115200\0" \
+  "usb_init=usb start\0" \
+  "usb_device=0:1\0" \
+  "usb_root=/dev/sda1\0" \
+  "usb_rootfstype=ext2\0" \
+  "usb_rootdelay=10\0"\
+  \
+  "mtdparts=mtdparts=orion_nand:1M(u-boot),4M(uImage),32M(rootfs),-(data)\0"\
+  "mtdids=nand0=orion_nand\0"\
+  "partition=nand0,2\0"\
+  \
+  "bootcmd_pogo=fsload uboot-original-mtd0.kwb; go 0x800200\0" \
+  \
+  /* Zero the first bit at 0x800000 to clear any old image still in RAM after a warm reboot */\
+  "usb_load_uimage=mw 0x800000 0 1; ext2load usb $usb_device 0x800000 /boot/uImage\0"\
+  "usb_boot="\
+    "if ext2load usb $usb_device 0x1100000 /boot/uInitrd; then"\
+    " bootm 0x800000 0x1100000;"\
+    "else"\
+    " bootm 0x800000;"\
+    "fi;\0"\
+  \
+  "set_bootargs_usb=setenv bootargs console=$console root=$usb_root rootdelay=$usb_rootdelay rootfstype=$usb_rootfstype $mtdparts\0" \
+  "bootcmd_usb=run usb_init; run usb_load_uimage; run set_bootargs_usb; run usb_boot;\0" \
+  \
+  "led_init=green blinking\0" \
+  "led_exit=green off\0" \
+  "led_error=orange blinking\0"
+
+
+#define CONFIG_BOOTCOMMAND "run bootcmd_usb; usb stop; run bootcmd_pogo; reset"
+
+/*
+ * Size of malloc() pool
+ */
+#define CONFIG_SYS_MALLOC_LEN	(1024 * 1024) /* 1 MiB for malloc() */
+/* size in bytes reserved for initial data */
+#define CONFIG_SYS_GBL_DATA_SIZE	128
+
+/*
+ * Other required minimal configurations
+ */
+#define CONFIG_SYS_LONGHELP
+#define CONFIG_AUTO_COMPLETE
+#define CONFIG_CMDLINE_EDITING
+#define CONFIG_ARCH_CPU_INIT	/* call arch_cpu_init() */
+#define CONFIG_ARCH_MISC_INIT	/* call arch_misc_init() */
+#define CONFIG_DISPLAY_CPUINFO	/* Display cpu info */
+#define CONFIG_NR_DRAM_BANKS	4
+#define CONFIG_STACKSIZE	0x00100000	/* regular stack- 1M */
+#define CONFIG_SYS_LOAD_ADDR	0x00800000	/* default load adr- 8M */
+#define CONFIG_SYS_MEMTEST_START 0x00400000	/* 4M */
+#define CONFIG_SYS_MEMTEST_END	0x007fffff	/*(_8M -1) */
+#define CONFIG_SYS_RESET_ADDRESS 0xffff0000	/* Rst Vector Adr */
+#define CONFIG_SYS_MAXARGS	16	/* max number of command args */
+
+/*
+ * Ethernet Driver configuration
+ */
+#ifdef CONFIG_CMD_NET
+#define CONFIG_NETCONSOLE	/* include NetConsole support   */
+#define CONFIG_NET_MULTI	/* specify more that one ports available */
+#define	CONFIG_MII		/* expose smi ove miiphy interface */
+#define CONFIG_MVGBE		/* Enable Marvell Gbe Controller Driver */
+#define CONFIG_SYS_FAULT_ECHO_LINK_DOWN	/* detect link using phy */
+#define CONFIG_MVGBE_PORTS	{1, 0}	/* enable port 0 only */
+#define CONFIG_PHY_BASE_ADR	0
+#define CONFIG_ENV_OVERWRITE	/* ethaddr can be reprogrammed */
+#define CONFIG_RESET_PHY_R	/* use reset_phy() to init mv8831116 PHY */
+#endif /* CONFIG_CMD_NET */
+
+/*
+ * USB/EHCI
+ */
+#ifdef CONFIG_CMD_USB
+#define CONFIG_USB_EHCI			/* Enable EHCI USB support */
+#define CONFIG_USB_EHCI_KIRKWOOD	/* on Kirkwood platform	*/
+#define CONFIG_EHCI_HCD_INIT_AFTER_RESET
+#define CONFIG_EHCI_IS_TDI
+#define CONFIG_USB_STORAGE
+#define CONFIG_DOS_PARTITION
+#define CONFIG_ISO_PARTITION
+#define CONFIG_SUPPORT_VFAT
+#endif /* CONFIG_CMD_USB */
+
+/*
+ * IDe Support on SATA port0
+ */
+#ifdef CONFIG_CMD_IDE   
+#define __io
+#define CONFIG_MVSATA_IDE
+#define CONFIG_IDE_PREINIT
+#define CONFIG_MVSATA_IDE_USE_PORT1
+/* Needs byte-swapping for ATA data register */
+#define CONFIG_IDE_SWAP_IO
+/* Data, registers and alternate blocks are at the same offset */
+#define CONFIG_SYS_ATA_DATA_OFFSET     (0x0100)
+#define CONFIG_SYS_ATA_REG_OFFSET      (0x0100)
+#define CONFIG_SYS_ATA_ALT_OFFSET      (0x0100)
+/* Each 8-bit ATA register is aligned to a 4-bytes address */
+#define CONFIG_SYS_ATA_STRIDE          4
+/* Controller supports 48-bits LBA addressing */
+#define CONFIG_LBA48
+/* CONFIG_CMD_IDE requires some #defines for ATA registers */
+#define CONFIG_SYS_IDE_MAXBUS          2
+#define CONFIG_SYS_IDE_MAXDEVICE       2
+/* ATA registers base is at SATA controller base */
+#define CONFIG_SYS_ATA_BASE_ADDR       KW_SATA_BASE
+/* ATA bus 0 is Kirkwood port 0 on openrd */
+#define CONFIG_SYS_ATA_IDE0_OFFSET     KW_SATA_PORT0_OFFSET
+/* ATA bus 1 is Kirkwood port 1 on openrd */
+#define CONFIG_SYS_ATA_IDE1_OFFSET     KW_SATA_PORT1_OFFSET
+#endif /* CONFIG_CMD_IDE */
+
+/*
+ * File system
+ */
+#define CONFIG_CMD_EXT2
+#define CONFIG_CMD_FAT
+#define CONFIG_CMD_JFFS2
+#define CONFIG_JFFS2_NAND
+#define CONFIG_JFFS2_LZO
+#define CONFIG_CMD_UBI
+#define CONFIG_CMD_UBIFS
+#define CONFIG_RBTREE
+#define CONFIG_MTD_DEVICE               /* needed for mtdparts commands */
+#define CONFIG_MTD_PARTITIONS
+#define CONFIG_CMD_MTDPARTS
+#define CONFIG_LZO
+
+#define CONFIG_STATUS_LED
+#define CONFIG_BOARD_SPECIFIC_LED
+
+#undef CONFIG_SYS_FAULT_ECHO_LINK_DOWN // Fix
+
+#define STATUS_LED_GREEN   0
+#define STATUS_LED_RED     1
+
+#define STATUS_LED_BIT     STATUS_LED_GREEN
+#define STATUS_LED_STATE   STATUS_LED_OFF
+#define STATUS_LED_PERIOD  (CONFIG_SYS_HZ /4)
+
+#define STATUS_LED_BIT1    STATUS_LED_RED
+#define STATUS_LED_STATE1  STATUS_LED_OFF
+#define STATUS_LED_PERIOD1 (CONFIG_SYS_HZ /4)
+
+#define CONFIG_MISC_INIT_R
+
+#endif /* _CONFIG_GOFLEXNET_H */
